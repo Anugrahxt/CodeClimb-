821222a8547f434d8bc45eb874ebd6b8ce7c729f
Anugrahxt <129720195+Anugrahxt@users.noreply.github.com>
2025-06-11 23:51:03 +0530

    Create PalindromeString.java

---
+++ PalindromeString.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+public class PalindromeString {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter a string: ");
+        String str = sc.nextLine();
+        boolean isPalindrome = true;
+        
+        for (int i = 0; i < str.length() / 2; i++) {
+            if (str.charAt(i) != str.charAt(str.length() - 1 - i)) {
+                isPalindrome = false;
+                break;
+            }
+        }
+        System.out.println(isPalindrome ? "Palindrome" : "Not Palindrome");
+        sc.close();
+    }
+}
